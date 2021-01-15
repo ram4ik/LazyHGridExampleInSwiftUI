d5a3b6df83e3cf19a5e14dc5e5ce71135f9c6591
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-01-15 09:38:11 +0200

    Initial Commit

---
+++ LazyHGridExampleInSwiftUI/LazyHGridExampleInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyHGridExampleInSwiftUIApp.swift
+//  LazyHGridExampleInSwiftUI
+//
+//  Created by Ramill Ibragimov on 15.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct LazyHGridExampleInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
